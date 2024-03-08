4d6541465aaba48c2cb147e3ef1445811178b207
rem1xed <mykhailolypko2006@gmail.com>
2024-03-08 18:15:24 +0200

    added 3 and 5 function

--- source.cpp
+++ source.cpp
@@ -2,6 +2,20 @@
 
 using namespace std;
 
+int multiplication(int a, int b)
+{
+	return a * b;
+}
+int pow(int number, int power)
+{
+	int result = number;
+	for (int i = 0; i < power - 1; i++)
+	{
+		result *= number;
+	}
+	return result;
+}
+
 void main()
 {
     cout << "hello";
