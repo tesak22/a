bfcfc34097dd743154add87afebcd4ba16f9b3af
rem1xed <mykhailolypko2006@gmail.com>
2024-03-08 18:44:07 +0200

    added 3 and 5 func

--- source.cpp
+++ source.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 #include <math.h>
 using namespace std;
+
+int multiplication(int a, int b)
+{
+    return a * b;
+}
+int pow(int number, int power)
+{
+    int result = number;
+    for (int i = 0; i < power - 1; i++)
+    {
+        result *= number;
+    }
+    return result;
+}
+
+
 void division(int a,int b) {
     int result = a / b;
     cout << "Result of division " << a << " and " << b << " = " << result;
