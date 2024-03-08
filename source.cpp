75f63810b8166b6d5dd0eb4df24547dfd6551f5b
StrixyGb <daryyyzdes1re@gmail.com>
2024-03-08 18:00:57 +0200

    Added a division and sqrt function

--- source.cpp
+++ source.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
-
+#include <math.h>
 using namespace std;
-
+void division(int a,int b) {
+    int result = a / b;
+    cout << "Result of division " << a << " and " << b << " = " << result;
+}
+void sqrtt(int a) {
+    cout << " Sqrt of number " <<a << " is equal " <<  sqrt(a);
+}
 void main()
 {
     cout << "hello";
+   
 }
